38adc60a1be97b121eab78a53fa5994e445fe98b
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-12-22 21:16:49 -0800

    Add files via upload

---
+++ Src/Scripts/Store.js
@@ -0,0 +1,24 @@
+const Enmap = require('enmap');
+
+module.exports = {
+    guildData: new Enmap({ name: 'guild-data' }),
+    clientSettings: new Enmap({ name: 'client-settings'}),
+    emotes: {
+        check: '<:utilidexPass:454452324413210624>',
+        x: '<:utilidexFail:454452324455284738>',
+        warn: '<:utilidexWarning:454452324807475222>',
+        connection: {
+            good: '<:utilidexGoodConnection:498224953863307274>',
+            slow: '<:utilidexSlowConnection:498224954072891421>',
+            bad: '<:utilidexBadConnection:498224953439813662>'
+        }
+    },
+    supportStaff: [],
+    disabledCommands: [],
+    blacklistedGuilds: [],
+    blacklistedUsers: []
+};
+
+//this.client.store.guildSettings.get(message.guild.id, 'prefix');
+//this.client.store.emotes.x;
+//this.client.store.emotes.connection.slow;
