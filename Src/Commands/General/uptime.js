45060ab68d61310cf9c217b3ddc3d9dfd8e2e8fd
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-12-23 22:08:44 -0800

    Update uptime.js

--- Src/Commands/General/uptime.js
+++ Src/Commands/General/uptime.js
@@ -18,15 +18,6 @@ module.exports = class extends Command {
     };
 
     async run(message, args) {
-        /**
-         let restarted = distanceInWordsToNow(new Date(this.client.readyAt), new Date());
-        const embed = new RichEmbed()
-            .setColor(this.client.color)
-            .setAuthor(this.client.user.username, this.client.user.displayAvatarURL)
-            .setTitle('Bot Uptime')
-            .setDescription(`The bot has been online since **${uptime}**\n» Last Restarted: ${restarted} ago`);
-        return message.channel.send(embed);
-         */
         const time = format(new Date(this.client.readyAt), 'MM/DD/YY, hh:mm A');
         const restarted = distanceInWordsToNow(new Date(this.client.readyAt), new Date());
         return message.responder.embed({
